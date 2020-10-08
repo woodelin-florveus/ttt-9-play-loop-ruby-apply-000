36cb40311500353955d0158a0ef36ecd099d723a
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-08 13:57:27 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,12 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+  # Start a loop and call #turn.
+  turn_count = 0
+  while turn < 9
+    turn(board)
+    turn += 1
+  end
+end
